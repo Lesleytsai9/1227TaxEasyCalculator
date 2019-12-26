6823c93b9c332b03464ec4bfd4fd7a8cefb0693a
Lesleytsai9 <e861k91677@gmail.com>
2019-12-26 15:26:21 +0800

    Initial Commit

---
+++ 1226tipTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1226tipTest
+//
+//  Created by 蔡家雯 on 2019/12/26.
+//  Copyright © 2019 lesley tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
